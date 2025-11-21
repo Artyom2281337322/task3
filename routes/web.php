598d6cbbaebdafb5effe55fdb12a3c3df8b81fc7
Студенческая <@rbiu.local>
2025-11-21 09:27:01 +0500

    6 начало

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,27 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+    Route::get('/reports', [ReportController::class, 'index'])
+        ->name('report.index');
+
+
+    Route::get('/reports/create', function () {
+        return view('report.create');
+    })->name('reports.create');
+
+    Route::delete('/reports/{report}', [ReportController::class, 'destroy'])
+        ->name('reports.destroy');
+
+    Route::post('/reports', [ReportController::class, 'store'])
+        ->name('reports.store');
+
+    Route::get('/reports/{report}/edit', [ReportController::class, 'edit'])
+        ->name('reports.edit');
+
+    Route::put('/reports/{report}', [ReportController::class, 'update'])
+        ->name('reports.update');
 });
 
-Route::get('/reports', [ReportController::class, 'index'])
-        -> name('report.index');
-   
-
-Route::get('/reports/create', function(){
-    return view('report.create');
-}) -> name('reports.create');
-
-Route::delete('/reports/{report}', [ReportController::class, 'destroy'])
-   -> name('reports.destroy');
-
-Route::post('/reports', [ReportController::class, 'store'])
-   -> name('reports.store');
-
-Route::get('/reports/{report}/edit', [ReportController::class, 'edit'])
-    -> name('reports.edit');
-
-Route::put('/reports/{report}', [ReportController::class, 'update'])
-    -> name('reports.update');
-
-require __DIR__.'/auth.php';
 
 
+require __DIR__ . '/auth.php';
